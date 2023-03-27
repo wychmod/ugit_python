c0772737297ee5d56c55ad85c0f4ce0f05d5f508
wychmod <545480453@qq.com>
2023-03-28 00:10:35 +0800

    feat: 使用安全散列算法1，将文件内容哈希，然后将哈希值作为文件名进行保存。

--- ugit/data.py
+++ ugit/data.py
@@ -1,7 +1,22 @@
+import hashlib
 import os
 
 GIT_DIR = '.ugit'
+GIT_OBJECT_DIR = os.path.join(GIT_DIR, "objects")
 
 
 def init():
     os.makedirs(GIT_DIR)
+    os.makedirs(GIT_OBJECT_DIR)
+
+
+def hash_object(data):
+    """
+    创建对象文件，写入文件
+    :param data: 文件数据
+    :return: oid值
+    """
+    oid = hashlib.sha1(data).hexdigest()
+    with open(os.path.join(GIT_OBJECT_DIR, oid), 'wb') as out:
+        out.write(data)
+    return oid
