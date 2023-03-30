3249dfc41198e5b1c31070144d26d2b92a854209
wychmod <545480453@qq.com>
2023-03-31 00:02:59 +0800

    feat: 忽略 .ugit 文件

--- ugit/base.py
+++ ugit/base.py
@@ -2,12 +2,19 @@ import os
 
 from . import data
 
+
 def write_tree(directory='.'):
     with os.scandir(directory) as it:
         for entry in it:
             full = os.path.join(directory, entry.name)
+            if is_ignored(full):
+                continue
             if entry.is_file(follow_symlinks=False):
                 # TODO write the file to object store
                 print(full)
             elif entry.is_dir(follow_symlinks=False):
                 write_tree(full)
+
+
+def is_ignored(path):
+    return '.ugit' in path.split('/')
